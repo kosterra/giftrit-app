670138559f7d146d0a619b3fc9fb6768d14a3c56
Ralph Koster <ralph.koster@clavisit.com>
2017-10-29 22:18:40 +0100

    Some Tests with hello world

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWorld from './components/HelloWorld';
-
+import './index.css';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<HelloWorld />, document.getElementById('content'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
