add240a74d8a3f064159d78717892da462883f95
Ralph Koster <ralph.koster@clavisit.com>
2017-10-29 22:16:32 +0100

    Some Tests with hello world

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SimpleCounter from './components/SimpleCounter';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          Hello World!
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <p>Hello world!</p>
+                <SimpleCounter />
+            </div>
+        );
+    }
 }
 
 export default App;
