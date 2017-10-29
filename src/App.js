e8d10450511e4910884d4e2fb4f86fa16aeb6bc2
Ralph Koster <ralph.koster@clavisit.com>
2017-10-29 22:21:37 +0100

    Some Tests with hello world

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import SimpleCounter from './components/SimpleCounter';
 class App extends Component {
     render() {
         return (
-            <div>
-                <p>Hello world!</p>
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    Hello World!
+                </p>
+
                 <SimpleCounter />
             </div>
         );
