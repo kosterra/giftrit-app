47bd3d388e018dac32eeee4aa8f00c6352f09750
Ralph Koster <ralph.koster@clavisit.com>
2018-01-03 14:29:25 +0100

    add gift list to public page

---
+++ src/components/giftlist.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const GiftList = () => (
+
+    <div className="gift-list">
+        This is where all gifts are shown as a list
+    </div>
+);
+
+export default GiftList;
--- src/pages/public.js
+++ src/pages/public.js
@@ -2,13 +2,14 @@ import React from 'react';
 import NavHelper from '../components/nav-helper'
 import Header from '../components/header'
 import Footer from '../components/footer'
+import GiftList from '../components/giftlist'
 
 export default class PublicPage extends React.Component {
   render() {
     return (
       <NavHelper className='container'>
         <Header />
-        <div>Show a list of gifts at this point</div>
+        <GiftList />
         <Footer />
       </NavHelper>
     )
