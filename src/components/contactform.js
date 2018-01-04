8b815e037014a8810b67826c456843bc426e37a8
Ralph Koster <ralph.koster@clavisit.com>
2018-01-04 17:27:23 +0100

    Trying to implement authorization for contactform

--- src/components/contactform.js
+++ src/components/contactform.js
@@ -13,7 +13,7 @@ export default class ContactForm extends React.Component {
             headers: {
                 'Content-Type': 'application/json',
                 'accept': 'application/json',
-                'Authorization': 'Bearer ' + localStorage.getItem('access_token')
+                'Authorization': 'Bearer ' + JSON.parse(window.localStorage['access_token'])
             },
             body: JSON.stringify({
                 from: this.state.from,
