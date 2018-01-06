8203e2e363c609ed5af9745a84b64f623abc0eee
Ralph Koster <ralph.koster@clavisit.com>
2018-01-06 22:04:20 +0100

    Added file upload to gift form

--- src/components/giftform.js
+++ src/components/giftform.js
@@ -1,7 +1,11 @@
 import React from 'react'
-import Dropzone from 'react-dropzone'
 
 import '../stylesheets/components/_giftform.scss';
+
+import filestack from 'filestack-js';
+
+/*const fsClient = filestack.init(process.env.FILEPICKER_API_KEY);*/
+const fsClient = filestack.init('ARg8PICxRfosu5qSBr0lQz');
 
 const url = 'https://giftrit-service.herokuapp.com/api/gifts';
 
@@ -16,14 +20,11 @@ export default class GiftForm extends React.Component {
             created: new Date().toISOString().slice(0,10),
             modified: new Date().toISOString().slice(0,10),
             userId: 2,
-            karma: 85
+            karma: 0,
+            userKarma: 0,
+            imageUrl: null,
+            imageMetadata: null
         }
-    }
-
-    onDrop(files) {
-        this.setState({
-            files: files
-        });
     }
 
     handleSubmit = e => {
@@ -41,7 +42,8 @@ export default class GiftForm extends React.Component {
                 created: this.state.created,
                 modified: this.state.modified,
                 userId: 2,
-                karma: 85
+                karma: this.state.karma,
+                imageUrl: this.state.imageUrl
             })
         }).then(response => {
             if (!response.ok) {
@@ -56,6 +58,8 @@ export default class GiftForm extends React.Component {
                 created: this.state.created,
                 modified: this.state.modified,
                 userId: 0,
+                karma: 0,
+                userKarma: 0,
                 type: 'success',
                 message: 'Thank you for creating a new gift.'
             });
@@ -68,6 +72,8 @@ export default class GiftForm extends React.Component {
                 created: this.state.created,
                 modified: this.state.modified,
                 userId: 2,
+                karma: 0,
+                userKarma: 0,
                 type: 'danger',
                 message: 'Failed to create a new gift. Please try again or contact us via contact form.'
             });
@@ -75,39 +81,90 @@ export default class GiftForm extends React.Component {
         });
 
         e.preventDefault();
+    };
+
+    handleChange = e => {
+        if (e.target.name === 'amount') {
+            this.calculateKarma(e.target.value);
+        }
+
+        this.setState({ [e.target.name]: e.target.value })
+    };
+
+    calculateKarma(amount) {
+        fetch('https://giftrit-service.herokuapp.com/api/karmas')
+            .then(res => res.json())
+            .then(data => {
+                let karma = 0;
+                let userKarma = this.state.userKarma;
+                let karmas = data.data;
+
+                for (let i = 0; i < karmas.length; i++) {
+                    let karmaObj = karmas[i];
+
+                    if (parseInt(karmaObj.amount) <= amount) {
+                        karma = karmaObj.karmapoints;
+                        userKarma = (userKarma - karmaObj.karmapoints);
+                    }
+                }
+
+                this.setState({ karma : karma, userKarma : userKarma });
+            });
     }
 
-    handleChange = e => this.setState({ [e.target.name]: e.target.value })
+    /*
+     * From here this are the methods to upload images to filestack
+     */
+    uploadImage = () => {
+        return fsClient.pick(
+            {
+                accept: 'image/*',
+                maxSize: 1024 * 1024 * 2
+            }
+        );
+    };
+
+    getMetadata = (handle) => {
+        fsClient.metadata(handle)
+            .then(metadata => this.setState({ imageMetadata : metadata }))
+            .catch(err => console.log(err));
+    };
+
+    setPicture = () => {
+        this.uploadImage()
+            .then(data => {
+                const { url, handle } = data.filesUploaded[0];
+                this.setState({ imageUrl : url });
+                this.getMetadata(handle);
+                console.log(JSON.stringify(data.filesUploaded));
+            })
+            .catch(err => console.log(err));
+    };
 
     render() {
+        const imageUrl = this.state.imageUrl;
+
         return (
             <div className="gift-form-container">
                 <form className="gift-form" onSubmit={this.handleSubmit}>
                     <div className="gift-form-inputs">
                         <div className="gift-form-images">
-                            <div className="dropzone">
-                                <Dropzone accept="image/jpeg, image/png"
-                                          onDrop={this.onDrop.bind(this)}>
-                                    {({ isDragActive, isDragReject, acceptedFiles, rejectedFiles }) => {
-                                        if (isDragActive) {
-                                            return "This file is authorized";
-                                        }
-                                        if (isDragReject) {
-                                            return "This file is not authorized";
-                                        }
-                                        return acceptedFiles.length || rejectedFiles.length
-                                            ? `Accepted ${acceptedFiles.length}, rejected ${rejectedFiles.length} files`
-                                            : "Drag images here to visualize your gift";
-                                    }}
-                                </Dropzone>
+                            <div className="thumbnail">
+                                <img
+                                    className="img-responsive"
+                                    src={imageUrl || 'http://placehold.it/800x600?text=Upload+a+Photo'}
+                                    alt="the gift"
+                                />
                             </div>
-                            <aside>
-                                <ul>
-                                    {
-                                        this.state.files.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
-                                    }
-                                </ul>
-                            </aside>
+                            <div className="text-center">
+                                <button
+                                    type="button"
+                                    className="btn btn-filestack"
+                                    onClick={this.setPicture}
+                                >
+                                    <i className="glyphicon glyphicon-upload" /> Upload
+                                </button>
+                            </div>
                         </div>
                         <div className="gift-form-data">
                             <h2>State your wish...</h2>
@@ -116,7 +173,7 @@ export default class GiftForm extends React.Component {
                                 <input type="text" name="title" value={this.state.title} onChange={this.handleChange} placeholder="Enter a name for your gift" required />
                             </div>
                             <div className="gift-amount">
-                                <input type="number" name="amount" value={this.state.amount} onChange={this.handleChange} placeholder="CHF" required />
+                                <input type="number" name="amount" min={0} max={10000} value={this.state.amount} onChange={this.handleChange} placeholder="CHF" required />
                                 <span>Enter the max amount of money you need to get this gift</span>
                             </div>
                             <div className="gift-description">
@@ -124,8 +181,12 @@ export default class GiftForm extends React.Component {
                         </div>
                     </div>
                     <div className="gift-form-submit">
-                        <div className="gift-gkp">This wish requires <span>150 gkp</span></div>
-                        <div className="gift-gkp">After you stated your wish, you will have 100 bkp</div>
+                        <div className="gift-gkp">This wish requires <span className="karma gkp">{this.state.karma > 0 ? this.state.karma : 0} gkp!</span></div>
+                        <div className="gift-gkp">After you stated your wish, you will have&nbsp;
+                            <span className={"karma " + (100 > 0 ? 'gkp' : 'bkp')}>
+                                {100} {100 > 0 ? 'gkp!' : 'bkp!'}
+                            </span>
+                        </div>
                         <button>That sounds fair!</button>
                         <a href="/" className="cancel">Cancel</a>
                     </div>
