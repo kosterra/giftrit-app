3d897b59c516d5b164ddd3d279042f5c153bf596
rudzitis <rudzitis@hispeed.ch>
2018-01-07 21:25:35 +0100

    Link korrigiert, damit Login auch auf Unterseiten funktioniert

--- src/components/loginbuttons.js
+++ src/components/loginbuttons.js
@@ -3,8 +3,8 @@ import React from 'react';
 const LoginButtons = () => (
     <div className="login-buttons">
         <ul>
-            <li><a href="login" className="loginbutton">Login</a></li>
-            <li><a href="login">Sign up</a></li>
+            <li><a href="/login" className="loginbutton">Login</a></li>
+            <li><a href="/login">Sign up</a></li>
         </ul>
     </div>
 );
